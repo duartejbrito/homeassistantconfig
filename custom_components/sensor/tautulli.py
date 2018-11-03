0d9ecd63f84d2da965e820f37f5715bfe3029260
Duarte Brito <sytertzp@gmail.com>
2018-11-03 23:27:11 +0000

    .

--- custom_components/sensor/tautulli.py
+++ custom_components/sensor/tautulli.py
@@ -110,9 +110,10 @@ class TautulliSensor(Entity):
             self._attributes['top_user'] = self.home[7]['rows'][0]['user']
         index = 0 
         self._attributes['sessions'] = {}
-        for item in self.sessions['sessions']:
+        self._attributes['sessions'] = self.sessions['sessions']
+        """for item in self.sessions['sessions']:
             self._attributes['sessions'][index] = item
-            index += 1
+            index += 1"""
         for key in self.sessions:
             if 'sessions' not in key:
                 self._attributes[key] = self.sessions[key]
