0a84fe463f25be83374129f4954e1f0e554eb0fc
Duarte Brito <sytertzp@gmail.com>
2018-11-04 13:01:50 +0000

    .

--- www/lovelace-player/lovelace-player-config.js
+++ www/lovelace-player/lovelace-player-config.js
@@ -3,7 +3,8 @@ setTimeout(function() {
     // Add your bindings here
     // Example:
     // LovelacePlayer.bind("media_player.kitchen_dashboard", "ff90349d-4a6eb8b1");
+    LovelacePlayer.bind("media_player.lovelace_player_pc", "8ac6a295-aad4c7d0");
   
     // Remove this line when you found your Device ID
-    LovelacePlayer.debug();
+    // LovelacePlayer.debug();
   }, 200);
